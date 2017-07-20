a0ba5a563f84786462009fb94d2aabdd6cf011da
tanmillet <tanmillet@sina.com>
2017-07-20 17:26:55 +0800

    init pro

---
+++ hasher/src/LucasMD5.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Terry Lucas
+ * Date: 2017/7/20
+ * Time: 16:43
+ */
+
+namespace Lucas\Hasher;
+
+/**
+ * Class LucasMD5
+ * User: Terry Lucas
+ * @package Lucas\Hasher
+ */
+class LucasMD5
+{
+    /**
+     * User: Terry Lucas
+     * @param $value
+     * @param array $options
+     * @return string
+     */
+    public function make($value, array $options = [])
+    {
+        $salt = isset($options['salt']) ? $options['salt'] : '';
+
+        return hash('md5', $value, $salt);
+    }
+
+    /**
+     * User: Terry Lucas
+     * @param $value
+     * @param $hashValue
+     * @param array $options
+     * @return bool
+     */
+    public function check($value, $hashValue, array $options = [])
+    {
+        $salt = isset($options['salt']) ? $options['salt'] : '';
+
+        return hash('md5', $value, $salt) == $hashValue;
+    }
+}
---
+++ hasher/src/LucasMD5Provider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Lucas\Hasher;
+
+use Illuminate\Support\ServiceProvider;
+
+class LucasMD5Provider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+        $this->app->singleton('lucasmd5', function () {
+
+            return new LucasMD5();
+        });
+    }
+}
